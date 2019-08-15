893999ffbeb16a97b278ecea079cdab373e4d7f2
adrimrutra <amir@isd.dp.ua>
2019-08-15 10:08:39 +0300

    fix

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/test4heroku'));
+app.use(express.static(__dirname + '/dist'));
 
 app.all('*', (req, res) => {  
-    res.status(200).sendFile(__dirname + '/dist/test4heroku/index.html');  
+    res.status(200).sendFile(__dirname + '/dist/index.html');  
   });  
 app.listen(process.env.PORT || 8080);
